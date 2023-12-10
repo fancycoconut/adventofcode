c1de4c9dc809eaf4a8bf19f8363a729e5c8665d5
Kawai Wong <kawai.wong@pushpay.com>
2023-12-10 17:09:55 +1100

    Day 4 Part 2

--- 2023/day4/Program.cs
+++ 2023/day4/Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine("Hello, World!");
 
 Part1("sample.txt");
 Part1("input.txt");
+Part2("sample.txt");
+Part2("input.txt");
 
 void Part1(string filename)
 {
@@ -33,6 +35,52 @@ void Part1(string filename)
     }
     
     Console.WriteLine($"Part 1 total number of points: {sum}");
+}
+
+void Part2(string filename)
+{
+    // key: card #, value: number of cards won
+    var memoizedCardsWon = new Dictionary<int, int>();
+    var copiesOfWonCardIndices = new Queue<int>();
+    
+    var lines = File.ReadAllLines(filename);
+
+    var totalNumberOfCardsWon = lines.Length;
+
+    var i = 1;
+    foreach (var line in lines)
+    {
+        var winningNumbers = GetWinningNumbers(line);
+        var actualNumbers = GetActualNumbers(line);
+        
+        var numberOfCardsWon = actualNumbers.Sum(number => winningNumbers.Contains(number)
+            ? 1
+            : 0);
+        memoizedCardsWon[i] = numberOfCardsWon;
+
+        for (var j = 1; j <= numberOfCardsWon; j++)
+        {
+            var cardWon = i + j;
+            copiesOfWonCardIndices.Enqueue(cardWon);
+            totalNumberOfCardsWon += 1;
+        }
+        
+        i++;
+    }
+    
+    while (copiesOfWonCardIndices.Count != 0)
+    {
+        var cardWon = copiesOfWonCardIndices.Dequeue();
+        var numberOfCardsWon = memoizedCardsWon[cardWon];
+        
+        for (var j = 1; j <= numberOfCardsWon; j++)
+        {
+            copiesOfWonCardIndices.Enqueue(cardWon+j);
+            totalNumberOfCardsWon += 1;
+        }
+    }
+    
+    Console.WriteLine($"Part 2 total number of cards won: {totalNumberOfCardsWon}");
 }
 
 HashSet<int> GetWinningNumbers(string line)
