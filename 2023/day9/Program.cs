ed38625536b687850a931b888e6c0414ab1f9a64
Kawai Wong <kawai.wong@pushpay.com>
2023-12-21 11:01:27 +1300

    Day 9 Part 1

--- 2023/day9/Program.cs
+++ 2023/day9/Program.cs
@@ -1,25 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Part1("sample.txt");
+Part1("input.txt");
+
 void Part1(string filename)
 {
     var lines = File.ReadLines(filename);
+
+    var sum = 0;
+    foreach (var line in lines)
+    {
+        var nextValue = PredictNextValue(line);
+        sum += nextValue;
+    }
+    
+    Console.WriteLine($"Part 1 - Sum of all extrapolated values: {sum}");
 }
 
 int PredictNextValue(string line)
 {
-    var predictedValues = new List<int>();
-    
     var initialValues = line.Split(" ")
         .Select(x => int.Parse(x))
         .ToArray();
-
-    var placeholders = new List<int>();
-    while (true)
+    var lastItemFromInitialValues = initialValues.Last();
+    
+    var placeholders = new List<List<int>>();
+    
+    List<int> currentPlaceholders;
+    do
     {
-        for (var i = 0; i <= initialValues.Length; i++)
+        currentPlaceholders = new List<int>();
+        for (var i = 1; i < initialValues.Length; i++)
         {
-            var placeholder = initialValues[i + 1] - initialValues[i];
+            var placeholder = initialValues[i] - initialValues[i - 1];
+            currentPlaceholders.Add(placeholder);
         }
-    }
+
+        initialValues = currentPlaceholders.ToArray();
+        placeholders.Add(currentPlaceholders);
+    } while (currentPlaceholders.All(x => x == 0) == false);
+
+    var predictedValue = placeholders.Select(x => x.Last())
+        .Sum() + lastItemFromInitialValues;
+
+    return predictedValue;
 }
