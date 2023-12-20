1972cdb57b1ca8f6e3610170ed7b2d994a0e4cb3
Kawai Wong <kawai.wong@pushpay.com>
2023-12-20 16:17:11 +1300

    WIP

--- 2023/day9/Program.cs
+++ 2023/day9/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+void Part1(string filename)
+{
+    var lines = File.ReadLines(filename);
+}
+
+int PredictNextValue(string line)
+{
+    var predictedValues = new List<int>();
+    
+    var initialValues = line.Split(" ")
+        .Select(x => int.Parse(x))
+        .ToArray();
+
+    var placeholders = new List<int>();
+    while (true)
+    {
+        for (var i = 0; i <= initialValues.Length; i++)
+        {
+            var placeholder = initialValues[i + 1] - initialValues[i];
+        }
+    }
+}
