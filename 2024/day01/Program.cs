bdfd43af0a2d42a57feed61a3ffeafef4e1ccae8
fancycoconut <zodiacdagreat@outlook.com>
2024-12-01 23:22:27 +1300

    Add day 1 part 2 solution

--- 2024/day01/Program.cs
+++ 2024/day01/Program.cs
@@ -5,6 +5,10 @@ Console.WriteLine("Hello, World!");
 var lines = File.ReadAllLines("input.txt");
 Part1(lines);
 
+//lines = File.ReadAllLines("sample.txt");
+lines = File.ReadAllLines("input.txt");
+Part2(lines);
+
 void Part1(string[] lines)
 {
   var list1 = ParseNumbersAtPosition(lines, 0)
@@ -24,6 +28,44 @@ void Part1(string[] lines)
   }
 
   Console.WriteLine($"Total distance: {totalDistance}");
+}
+
+void Part2(string[] lines)
+{
+    var list1 = ParseNumbersAtPosition(lines, 0).ToArray();
+    var list2 = ParseNumbersAtPosition(lines, 1).ToArray();
+
+    var numberOfOccurencesMap = GetNumberOfOccurences(list2);
+
+    var totalSimilarityScore = 0;
+    foreach (var number in list1)
+    {
+        numberOfOccurencesMap.TryGetValue(number, out var occurences);
+
+        var similarityScore = number * occurences;
+        totalSimilarityScore += similarityScore;
+    }
+
+    Console.WriteLine($"Total similarity score: {totalSimilarityScore}");
+}
+
+Dictionary<int, int> GetNumberOfOccurences(int[] numbers)
+{
+    var map = new Dictionary<int, int>();
+
+    foreach (var number in numbers)
+    {
+        if (map.ContainsKey(number))
+        {
+            map[number]++;
+        }
+        else
+        {
+            map[number] = 1;
+        }
+    }
+
+    return map;
 }
 
 IEnumerable<int> ParseNumbersAtPosition(string[] lines, int position)
