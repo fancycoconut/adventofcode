43617f9d292f1384ebdd71eeee4543be7c90bd3e
Kawai Wong <kawai.wong@pushpay.com>
2024-12-04 11:26:13 +1300

    Add solution for day 3 part 2

--- 2024/day03/Program.cs
+++ 2024/day03/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System.Text.RegularExpressions;
+using System.Text;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Part1("sample.txt");
-Part1("input.txt");
+//Part1("sample.txt");
+//Part1("input.txt");
+
+Part2("sample2.txt");
+Part2("input.txt");
 
 // xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 void Part1(string filename)
@@ -28,5 +32,50 @@ void Part1(string filename)
   }
 
   Console.WriteLine($"Part 1 - Total: {total}");
+}
+
+void Part2(string filename)
+{
+  const string pattern = @"mul\((-?\d+),\s*(-?\d+)\)|do\(\)|don't\(\)";
+
+  var raw = File.ReadAllText(filename);
+
+  var total = 0;
+  var matches = Regex.Matches(raw, pattern);
+  Console.WriteLine("Number of matches: {0}", matches.Count);
+
+  var multiplyIsOn = true;
+  foreach (Match match in matches)
+  {
+    if (match.Value == "do()")
+    {
+      Console.WriteLine("Multiply is on");
+
+      multiplyIsOn = true;
+      continue;
+    }
+
+    if (match.Value == "don't()")
+    {
+      Console.WriteLine("Multiply is off");
+
+      multiplyIsOn = false;
+      continue;
+    }
+
+    if (match.Value.StartsWith("mul") && multiplyIsOn)
+    {
+      Console.WriteLine(match.Value);
+
+      var operands = match.Value
+        .Replace("mul(", "")
+        .Replace(")", "")
+        .Split(",");
+
+      total += int.Parse(operands[0]) * int.Parse(operands[1]);
+    }
+  }
+
+  Console.WriteLine($"Part 2 - Total: {total}");
 }
 
--- 2024/day03/exercise.md
+++ 2024/day03/exercise.md
@@ -40,4 +40,4 @@ This time, the sum of the results is `48` `(2*4 + 8*5)`.
 
 Handle the new instructions; **what do you get if you add up all of the results of just the enabled multiplications?**
 
-Your Answer: `156388521`
+Your Answer: `75920122`
