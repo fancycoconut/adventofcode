2664c9472082f42379436398a34f0f6dbb1c8ff7
Kawai Wong <kawai.wong@pushpay.com>
2024-12-06 16:24:10 +1300

    Add day 5 part 2 solution

--- 2024/day05/Program.cs
+++ 2024/day05/Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Collections;Console.WriteLine("Hello, World!");
+using System.Collections;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
 
 //Part1("sample.txt");
 //Part1("input.txt");
 
 Part2("sample.txt");
+Part2("input.txt");
 
 void Part1(string filename)
 {
@@ -32,10 +36,16 @@ void Part1(string filename)
 void Part2(string filename)
 {
   var text = File.ReadAllText(filename);
-  var sections = text.Split("\r\n\r\n");
+  var sections = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) 
+    ? text.Split("\r\n\r\n")
+    : text.Split("\n\n");
 
-  var rules = sections[0].Split("\r\n");
-  var updates = sections[1].Split("\r\n");
+  var rules = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) 
+    ? sections[0].Split("\r\n")
+    : sections[0].Split("\n");
+  var updates = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) 
+    ? sections[1].Split("\r\n")
+    : sections[1].Split("\n");
   var rulesList = GetRulesList(rules);
 
   var total = 0;
@@ -46,7 +56,8 @@ void Part2(string filename)
     Console.WriteLine($"Update: `{update}` is correct? {isCorrect}");
     if (isCorrect) continue;
     
-    InsertionSort(pageNumbers, rulesList);
+    var comparer = new PrinterPageCorrectionComparer(pageNumbers, rulesList);
+    Array.Sort(pageNumbers, comparer);
     Console.WriteLine($"Sorted: {string.Join(",", pageNumbers)}");
     
     total += pageNumbers[pageNumbers.Length / 2];
@@ -118,4 +129,52 @@ List<(int left, int right)> GetRulesList(string[] rules)
   }
 
   return rulesList;
+}
+
+public class PrinterPageCorrectionComparer : IComparer<int>
+{
+  private readonly int[] _pageNumbers;
+  private readonly Dictionary<int, HashSet<int>> _rulesMap;
+
+  public PrinterPageCorrectionComparer(int[] pageNumbers, List<(int left, int right)> rulesList)
+  {
+    _pageNumbers = pageNumbers;
+    _rulesMap = BuildRulesMap(rulesList);
+  }
+  
+  public int Compare(int left, int right)
+  {
+    if (left == right) return 0;
+    if (IsBefore(left, right)) return -1;
+    if (IsBefore(right, left)) return 1;
+    return left.CompareTo(right);
+  }
+
+  private bool IsBefore(int left, int right)
+  {
+    if (!_rulesMap.TryGetValue(left, out var leftBeforeValues)) return false;
+      
+    if (leftBeforeValues.Contains(right)) return true;
+    return false;
+  }
+
+  private Dictionary<int, HashSet<int>> BuildRulesMap(List<(int left, int right)> rulesList)
+  {
+    var map = new Dictionary<int, HashSet<int>>();
+
+    foreach (var rule in rulesList)
+    {
+      if (map.ContainsKey(rule.left))
+      {
+        map[rule.left].Add(rule.right);
+      }
+      else
+      {
+        var set = new HashSet<int> { rule.right };
+        map[rule.left] = set;
+      }
+    }
+
+    return map;
+  }
 }
