c35c1d4fd81df45de9f2947f316d3cdf75e353de
Kawai Wong <kawai.wong@pushpay.com>
2024-12-07 22:03:59 +1000

    day 7 work in progress

--- 2024/day07/Program.cs
+++ 2024/day07/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+void Part1(string filename)
+{
+  var lines = File.ReadAllLines(filename);
+
+  var total = 0;
+  foreach (var line in lines)
+  {
+    var colonIndex = line.IndexOf(':');
+    var testValue = int.Parse(line[0..colonIndex]);
+    var numbers = line[(colonIndex + 1)..].Split(' ')
+      .Select(x => int.Parse(x))
+      .ToList();
+
+    Console.WriteLine($"{testValue}");
+  }
+
+  Console.WriteLine($"Part 1 - Total: {total}");
+}
+
